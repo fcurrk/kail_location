ad393d19bae14c872ed2eda15a39442e5dcc1283
fcurrk <48197714+fcurrk@users.noreply.github.com>
2026-06-30 14:46:13 +0800

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.ksp) apply false
+    // alias(libs.plugins.ksp) apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.7" apply false
 }
