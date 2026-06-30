35cae9ac9c48a7052b5ab01515ef9a823db597de
fcurrk <48197714+fcurrk@users.noreply.github.com>
2026-06-30 16:16:08 +0800

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp")
-    id("com.google.gms.google-services")
-    id("com.google.firebase.crashlytics")
+//    id("com.google.devtools.ksp")
+//    id("com.google.gms.google-services")
+//    id("com.google.firebase.crashlytics")
 }
 
 android {
